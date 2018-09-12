7102cf7fd0cd79aa61e5ee5f064f6e77af593133
OseiAlex <34588600+OseiAlex@users.noreply.github.com>
2018-09-12 17:38:30 +0100

    Update TicTacToe.java

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- *
- * @author MeneXia (Xavi Ablaza)
- *
- */
 public class TicTacToe {
     static Scanner in;
     static String[] board;
